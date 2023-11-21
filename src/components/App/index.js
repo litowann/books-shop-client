338751c0da795bdff808c4c462787d4fcd1ef6d2
Oleh Khmelevskyi <133887523+litowann@users.noreply.github.com>
2023-11-21 22:14:43 +0200

    set proxy

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const App = () => {
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        const fetchData = () => {
+            fetch("http://127.0.0.1:5000/books", {
+                method: 'GET',
+                withCredentials: true,
+                crossorigin: true,
+                mode: 'no-cors',
+            })
+            .then(response => response.json())
+            .then(data => setData(data))
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <div>App is ready</div>
     )
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { HttpsProxyAgent } = require('https-proxy-agent')
 
 module.exports = {
   mode: 'development',
@@ -18,6 +19,18 @@ module.exports = {
             ]
           }
         }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline'
       }
     ]
   },
@@ -31,11 +44,27 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Hello Webpack bundled JavaScript Project',
-      template: path.resolve(__dirname, './public/index.html'),
+      template: './public/index.html'
     })
   ],
   devServer: {
     static: path.resolve(__dirname, './dist'),
+    hot: true,
+    host: '0.0.0.0',
+    port: 3010,
+    historyApiFallback: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+    },
+    proxy: {
+      '/api': {
+        agent: new HttpsProxyAgent('localhost:5000'),
+        target: 'localhost:5000',
+        pathRewrite: { '^/api': '' },
+        changeOrigin: true,
+      },
+    },
   },
 };
