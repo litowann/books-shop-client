0394e5169598dbdf987eb8529fb1b322a390dbe1
Oleh Khmelevskyi <133887523+litowann@users.noreply.github.com>
2023-11-22 00:59:26 +0200

    update package json file

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const { HttpsProxyAgent } = require('https-proxy-agent')
 
 module.exports = {
   mode: 'development',
