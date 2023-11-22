62b4154639a4767978632b43f740720d9ee6d63e
Oleh Khmelevskyi <133887523+litowann@users.noreply.github.com>
2023-11-22 21:57:51 +0200

    update endpoint url

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,19 +1,25 @@
 import React, { useEffect, useState } from 'react';
+import { isEmpty } from 'lodash';
 
 const App = () => {
     const [data, setData] = useState({});
 
 
     useEffect(() => {
-        fetch('http://127.0.0.1:5000/api/books').then(response => response.json()).then(data => setData(data));
+        fetch('http://127.0.0.1:8000/books/').then(response => response.json()).then(data => setData(data));
     }, []);
+
+    if (isEmpty(data)) return <span>Loading...</span>
 
     return (
         <div>
             <ul>
-                {data?.books?.map(book => (
-                    <li key={book}>
-                        {book}
+                {data?.map(book => (
+                    <li key={book.year + book.pages}>
+                        <p>{book.name}</p>
+                        <p>{book.author}</p>
+                        <p>{book.pages}</p>
+                        <p>{book.year}</p>
                     </li>
                 ))}
             </ul></div>
